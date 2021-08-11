2ed5191bbaf7db1650593aad9b55225f9bb18f82
Arthurkettor) <arthurkettor@yahoo.com>
2021-08-10 19:46:27 -0700

    come

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I am Arthur and i am praticing DevOps"); 
+        System.out.println("Hello, World! I am Arthur and day one training DevOps"); 
     }
 }
